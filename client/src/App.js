056fc7ac8c77d489505f286bce8b0ecb1ebb9f98
Selina Feng <selina.feng@berkeley.edu>
2020-04-23 05:00:51 -0700

    styling using material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Visualization from './Visualization'
+import React from 'react';
 import './App.css';
+import Dashboard from './Dashboard';
 
 function App() {
-
-  useEffect(() => {
-    // TODO: Fetch all our saved visualizations
-  })
-
-  const addEntry = () => {
-    const entry = {
-      state: state,
-      days: days,
-    }
-    setEntries([entry, ...entries])
-    // TODO: Save this visualization somewhere
-  }
-
-  const displayEntries = () => {
-    const output = []
-    entries.forEach(e => {
-      output.push(<Visualization state={e.state} days={e.days}/>)
-    })
-    return output
-  }
-
-  const [entries, setEntries] = useState([])
-  const [state, setState] = useState("")
-  const [days, setDays] = useState("")
-
   return (
     <div className="App">
-      <h1>Visualizations</h1>
-      <div>
-        <input type="text" placeholder="state" onChange={e => setState(e.target.value)} />
-      </div>
-      <div>
-        <input type="text" placeholder="# of days" onChange={e => setDays(e.target.value)} />
-      </div>
-      <button onClick={addEntry} className="add">Add Visualization</button>
-      <div>
-        {displayEntries()}
-      </div>
+      <Dashboard />
     </div>
   );
 }
