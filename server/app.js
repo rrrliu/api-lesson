741a6a165e622a16587392f76291bd760b054922
Richard Liu <richard.y.liu@berkeley.edu>
2020-04-23 11:56:26 -0700

    server starter

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors())
 
 // Feel free to change port
 const port = 8000;
 
+// Jank
+const entries = []
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.get('/entries', (req, res) => {
+  res.send(entries);
+  // TODO: Mongo shit
+})
+
+app.post('/save', (req, res) => {
+  entries.push(req.body)
+  // TODO: Mongo shit
+  console.log('Saved ' + req.body)
+  res.send('Successfully saved!');
+})
 
 app.listen(port, () => console.log(`App running on port ${port}`));
