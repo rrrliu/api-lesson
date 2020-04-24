52fb7891dfcc6d5c9a60490cdf6a1a4ec9c78285
Kelvin Jue <kelvinjue@berkeley.edu>
2020-04-24 15:09:49 -0500

    made styling consistent with solution branch

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,21 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import Visualization from './Visualization';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Visualization from "./Visualization";
+import "./App.css";
 import { withStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, Container, Card, CardHeader, CardContent, CardActions, MenuItem, TextField, Divider, Button } from '@material-ui/core';
-import states from './us_states'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import {
+  AppBar,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Toolbar,
+  Typography,
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  MenuItem,
+  TextField,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import states from "./us_states";
 
 const styles = {
   card: {
     maxWidth: 500,
-    margin: "50px auto"
+    margin: "50px auto",
   },
   divider: {
     margin: "10px 0",
-  }, 
+  },
   cardActions: {
-    justifyContent: "center"
-  }, 
+    justifyContent: "center",
+  },
 };
 
 function InputCard(props) {
@@ -23,35 +40,33 @@ function InputCard(props) {
 
   useEffect(() => {
     // TODO: Fetch all our saved visualizations
-  })
+  });
 
   const addEntry = () => {
     const entry = {
       state: state,
       days: days,
-    }
-    setEntries([entry, ...entries])
+    };
+    setEntries([entry, ...entries]);
     // TODO: Save this visualization somewhere
-  }
+  };
 
   const displayEntries = () => {
-    const output = []
-    entries.forEach(e => {
-      output.push(<Visualization state={e.state} days={e.days}/>)
-    })
-    return output
-  }
-  
-  const [entries, setEntries] = useState([])
-  const [state, setState] = useState("")
-  const [days, setDays] = useState("")
+    const output = [];
+    entries.forEach((e) => {
+      output.push(<Visualization state={e.state} days={e.days} />);
+    });
+    return output;
+  };
+
+  const [entries, setEntries] = useState([]);
+  const [state, setState] = useState("");
+  const [days, setDays] = useState("");
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">
-            Visualizations
-          </Typography>
+          <Typography variant="h6">Visualizations</Typography>
         </Toolbar>
       </AppBar>
       <Container>
@@ -63,7 +78,7 @@ function InputCard(props) {
               select
               label="State"
               value={state}
-              onChange={e => setState(e.target.value)}
+              onChange={(e) => setState(e.target.value)}
               helperText="Please select a state"
             >
               {states.map((option) => (
@@ -73,25 +88,35 @@ function InputCard(props) {
               ))}
             </TextField>
             <Divider className={classes.divider} variant="middle" />
-            <TextField 
-              id="standard-basic" 
-              label="Number of days" 
+            <TextField
+              id="standard-basic"
+              label="Number of days"
               helperText="Please enter the number of days"
               value={days}
-              onChange={e => setDays(e.target.value)}
+              onChange={(e) => setDays(e.target.value)}
             />
           </CardContent>
           <CardActions className={classes.cardActions}>
-            <Button onClick={addEntry} color="primary">Add Visualization</Button>
+            <Button onClick={addEntry} color="primary">
+              Add Visualization
+            </Button>
           </CardActions>
         </Card>
-          
-        <div>
-          {displayEntries()}
-        </div>
+
+        <div>{displayEntries()}</div>
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            Past Visualizations
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails></ExpansionPanelDetails>
+        </ExpansionPanel>
       </Container>
     </div>
-  )
+  );
 }
 
 export default withStyles(styles)(InputCard);
