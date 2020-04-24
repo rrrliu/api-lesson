55e57d077ee06fe9af52ec6a777875f6b0929fd6
Kelvin Jue <kelvinjue@berkeley.edu>
2020-04-24 13:32:23 -0500

    cleaned up server code

--- server/app.js
+++ server/app.js
@@ -6,47 +6,34 @@ const bodyParser = require("body-parser");
 
 app.use(cors());
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-  bodyParser.json()
-);
+app.use(bodyParser.json());
 
 // Feel free to change port
 const port = 8000;
 
 let db;
 
-MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
+MongoClient.connect("mongodb://localhost:27017", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
   .then((client) => {
     console.log("Connected to Database");
     db = client.db("entry");
-    db.collection("entryCollection");
-    db.collection("entryCollection").insertOne({
-      name: "pastEntries",
-      pastEntries: [],
-    });
   })
   .catch((error) => console.error(error));
 
 app.get("/entries", (req, res) => {
-  db.collection("entryCollection").findOne({ name: "pastEntries" }, function (
-    err,
-    result
-  ) {
-    if (err) throw err;
-    console.log(result);
-    res.send(result.pastEntries);
-  });
+  db.collection("entryCollection")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.send(result);
+    });
 });
 
 app.post("/save", (req, res) => {
-  db.collection("entryCollection").updateOne(
-    { name: "pastEntries" },
-    { $push: { pastEntries: req.body } }
-  );
-  console.log("saved" + req.body);
+  db.collection("entryCollection").insertOne(req.body);
   res.send("Successfully saved!");
 });
 
