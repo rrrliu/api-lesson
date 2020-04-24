959efa340bcc60d60b7a8e63f7dced8e0c7515fa
kelvinjue <35481358+kelvinjue@users.noreply.github.com>
2020-04-23 23:39:40 -0500

    Revert "Solution"

--- client/src/Visualization.jsx
+++ client/src/Visualization.jsx
@@ -1,44 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { LineChart, AreaChart } from "react-chartkick";
-import "chart.js";
-import axios from "axios";
-import states from "./us_states.json";
+import React, { useEffect, useState } from 'react'
+import { LineChart, AreaChart } from 'react-chartkick'
+import 'chart.js'
+import axios from 'axios'
+import states from './us_states.json'
+
 
 export default function Visualization(props) {
-  const [data, setData] = useState({});
+	const [data, setData] = useState({})
 
-  useEffect(() => {
-    const endpoint = `https://covidtracking.com/api/v1/states/${props.state}/daily.json`;
-    axios
-      .get(endpoint)
-      .then((res) => {
-        const totalCases = { name: "Total cases", data: {} };
-        const newCases = { name: "New cases", data: {} };
-        res.data.slice(0, props.days).forEach((e) => {
-          totalCases.data[e.date] = e.positive;
-          newCases.data[e.date] = e.positiveIncrease;
-        });
-        setData([totalCases, newCases]);
-      })
-      .catch((err) => {
-        console.log(err);
-        alert("Invalid inputs");
-      });
-  });
+	useEffect(() => {
+		const endpoint = `https://covidtracking.com/api/v1/states/${props.state}/daily.json`
+		axios.get(endpoint)
+			.then(res => {
+				const totalCases = {name: "Total cases", data: {}}
+				const newCases = {name: "New cases", data: {}}
+				res.data.slice(0, props.days).forEach(e => {
+					totalCases.data[e.date] = e.positive
+					newCases.data[e.date] = e.positiveIncrease
+				});
+				setData([totalCases, newCases])
+			})
+			.catch(err => {
+				console.log(err)
+				alert("Invalid inputs")
+			});
+	});
 
-  const fullName = (abbr) => {
-    return states.filter((e) => e.abbreviation == abbr)[0].name;
-  };
+	const fullName = abbr => {
+		return states.filter(e => e.abbreviation == abbr)[0].name;
+	}
 
-  return (
-    <div>
-      <AreaChart
-        data={data}
-        xtitle="Date"
-        ytitle="Population"
-        min={0}
-        title={`Past ${props.days} days for ${fullName(props.state)}`}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<AreaChart data={data} xtitle="Date" ytitle="Population" min={0} title={`Past ${props.days} days for ${fullName(props.state)}`}/>
+		</div>
+	);
 }
